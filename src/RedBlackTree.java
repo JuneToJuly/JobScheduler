696ad451b8de49c33d670f16c54641cab86d0e25
JuneToJuly <itfairhope@aol.com>
2018-03-29 15:04:49 -0400

    Rotations have been added. Haven't Tested them. Need to do that and conclude what happens when we have a color change(doesn't fix the rotation). So, I would like to implement some loop that checks to see if the RedBlackTree property is now maintained.

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -10,7 +10,7 @@ public class RedBlackTree
 
 	public enum Child {LEFT, RIGHT}
 
-	public RedBlackNode head;
+	private RedBlackNode head;
 
 	public RedBlackTree()
 	{
@@ -59,7 +59,120 @@ public class RedBlackTree
 		}
 
 		InsertRotation insertRotation = classifyInsertRotation(insertNode);
+		performRotation(insertNode, insertRotation);
 
+	}
+
+	/*
+		Perform the rotations in accordance with the classified rotation
+	 */
+	private void performRotation(RedBlackNode insertNode, InsertRotation insertRotation)
+	{
+		// Switch time
+		switch (insertRotation)
+		{
+			// Nothing to do. This was the case for a black node. We have already set our parent
+			// and we have set ourselves to our parent's child.
+			case NONE:
+				break;
+			//  All 'r' are just color flips where we propigate the change up the tree
+			case RRr:
+				insertNode.getParent().setColor(Color.BLACK);
+				insertNode.getParent().getParent().setColor(Color.RED);
+				insertNode.getParent().getParent().getLeftChild().setColor(Color.BLACK);
+				break;
+			case RRb:
+				greatGrandUpdate(insertNode);
+				// Temp so we can have the parent's left
+				RedBlackNode parentLeft  = insertNode.getParent().getLeftChild();
+
+				// Set left child of parent to grand parent
+				insertNode.getParent().setLeftChild(insertNode.getParent().getParent());
+
+				// Set parent's parent to grandparent's old parent
+				insertNode.getParent().setParent(insertNode.getParent().getLeftChild().getParent());
+
+				// Set grandparent's parent to parent
+				insertNode.getParent().getLeftChild().setParent(insertNode.getParent());
+
+				// Set parent's old left child to the right of old grandparent
+				insertNode.getParent().getLeftChild().setRightChild(parentLeft);
+				break;
+			case LLr:
+				insertNode.getParent().setColor(Color.BLACK);
+				insertNode.getParent().getParent().setColor(Color.RED);
+				insertNode.getParent().getParent().getRightChild().setColor(Color.BLACK);
+				break;
+			case LLb:
+				greatGrandUpdate(insertNode);
+				// Temp so we can have the parent's right
+				RedBlackNode parentRight  = insertNode.getParent().getRightChild();
+
+				// Set right child of parent to grand parent
+				insertNode.getParent().setRightChild(insertNode.getParent().getParent());
+
+				// Set parent's parent to grandparent's old parent
+				insertNode.getParent().setParent(insertNode.getParent().getRightChild().getParent());
+
+				// Set grandparent's parent to parent
+				insertNode.getParent().getRightChild().setParent(insertNode.getParent());
+
+				// Set parent's old right child to the left of old grandparent
+				insertNode.getParent().getRightChild().setLeftChild(parentRight);
+				break;
+			case RLr:
+				insertNode.getParent().setColor(Color.BLACK);
+				insertNode.getParent().getParent().setColor(Color.RED);
+				insertNode.getParent().getParent().getLeftChild().setColor(Color.BLACK);
+				break;
+			case RLb:
+				greatGrandUpdate(insertNode);
+				// Set my parent's left to my right, and grandparent's right to my left
+				insertNode.getParent().getParent().setRightChild(insertNode.getLeftChild());
+				insertNode.getParent().setLeftChild(insertNode.getRightChild());
+
+				// Set my right to my parent, and my left to my grandparent
+				insertNode.setRightChild(insertNode.getParent());
+				insertNode.setLeftChild(insertNode.getParent().getParent());
+
+				// Hold onto my grandparent's old parent
+				RedBlackNode grandParentsParent = insertNode.getParent().getParent();
+
+				// Set my parent and grandparent to myself
+				insertNode.getRightChild().setParent(insertNode);
+				insertNode.getLeftChild().setParent(insertNode);
+
+				// Set my parent to my grandparent's old parent
+				insertNode.setParent(grandParentsParent);
+				break;
+			case LRr:
+				insertNode.getParent().setColor(Color.BLACK);
+				insertNode.getParent().getParent().setColor(Color.RED);
+				insertNode.getParent().getParent().getRightChild().setColor(Color.BLACK);
+				break;
+			case LRb:
+				greatGrandUpdate(insertNode);
+
+				// Set my parent's right to my left, and grandparent's left to my right
+				insertNode.getParent().getParent().setLeftChild(insertNode.getRightChild());
+				insertNode.getParent().setRightChild(insertNode.getLeftChild());
+
+				// Set my left to my parent, and my right to my grandparent
+				insertNode.setLeftChild(insertNode.getParent());
+				insertNode.setRightChild(insertNode.getParent().getParent());
+
+				// Hold onto my grandparent's old parent
+				RedBlackNode grandParentsParent = insertNode.getParent().getParent();
+
+				// Set my parent and grandparent to myself
+				insertNode.getLeftChild().setParent(insertNode);
+				insertNode.getRightChild().setParent(insertNode);
+
+				// Set my parent to my grandparent's old parent
+				insertNode.setParent(grandParentsParent);
+				break;
+				default:
+		}
 	}
 
 	/*
@@ -146,6 +259,32 @@ public class RedBlackTree
 			}
 
 		}
+		return NONE;
+	}
+
+	/**
+	 *  Userd in a lot of calls, just set the great grandparent to the current
+	 *  node when a rotation happens.
+	 * @param insertNode node to set great grand parent's child to
+	 * @return the child of the greatGrandparent
+	 */
+	private Child greatGrandUpdate(RedBlackNode insertNode)
+	{
+		Child greatToGrand = insertNode.getParent().getParent().getKey() >
+				insertNode.getParent().getParent().getParent().getKey()
+				? Child.RIGHT
+				: Child.LEFT;
+
+		// Need to set the child of great grand parent to our node
+		if(greatToGrand == Child.RIGHT)
+		{
+			insertNode.getParent().getParent().getParent().setRightChild(insertNode);
+		}
+		else
+		{
+			insertNode.getParent().getParent().getParent().setLeftChild(insertNode);
+		}
+		return greatToGrand;
 	}
 
 
